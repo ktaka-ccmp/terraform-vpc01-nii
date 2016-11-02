19d23611eb2b223c1a8cab9264ced38bd42f6861
Kimitoshi Takahshi <ktaka@ccmp.jp>
2016-11-02 17:00:57 +0900

    Added recipe for instances

---
+++ host_gen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+host_min=10
+host_max=15
+
+for i in $(seq $host_min $host_max) ; do
+
+host=v$(printf %03d $i)
+
+cat << EOF > ${host}.tf
+
+resource "aws_instance" "${host}" {
+	private_ip = "10.0.$i.0"
+	tags {
+        	Name = "ktaka_${host}"
+		Owner = "ktaka"
+    	}
+
+        ami = "ami-2a34e94a"
+        instance_type = "t2.micro"
+        subnet_id = "\${aws_subnet.priv.id}"
+	security_groups = ["\${aws_security_group.local.id}"]
+        source_dest_check = true
+        key_name = "ktaka.root"
+	iam_instance_profile = "ktaka_role"
+
+	root_block_device {
+		volume_type = "gp2"
+		volume_size = 10
+		delete_on_termination = "true"
+	}
+}
+
+EOF
+
+done
+
---
+++ v010.tf
@@ -0,0 +1,23 @@
+
+resource "aws_instance" "v010" {
+	private_ip = "10.0.10.0"
+	tags {
+        	Name = "ktaka_v010"
+		Owner = "ktaka"
+    	}
+
+        ami = "ami-2a34e94a"
+        instance_type = "t2.micro"
+        subnet_id = "${aws_subnet.priv.id}"
+	security_groups = ["${aws_security_group.local.id}"]
+        source_dest_check = true
+        key_name = "ktaka.root"
+	iam_instance_profile = "ktaka_role"
+
+	root_block_device {
+		volume_type = "gp2"
+		volume_size = 10
+		delete_on_termination = "true"
+	}
+}
+
