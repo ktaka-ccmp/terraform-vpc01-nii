1ac43fed551245a8536d95d725ff55103671f9dc
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-01-25 17:49:34 +0900

    Change in Security Group: Permit all traffic within same Security Group.

--- network.tf
+++ network.tf
@@ -37,17 +37,10 @@ resource "aws_security_group" "local" {
   vpc_id = "${aws_vpc.vpc.id}"
 
   ingress {
-      from_port = 0
-      to_port = 65535
-      protocol = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  ingress {
       from_port = 0 
       to_port = 0
       protocol = "-1"
-      cidr_blocks = ["10.0.0.0/16"]
+      self = true
   }
 
   egress {
